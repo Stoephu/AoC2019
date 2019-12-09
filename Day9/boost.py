9a3e44eb5b804e972092ae3416d13cf51cadf061
Stoephu <christoph.straehl@gmx.ch>
2019-12-09 12:46:04 +0100

    Update boost.py

--- Day9/boost.py
+++ Day9/boost.py
@@ -4,10 +4,16 @@ Created on Mon Dec  9 09:45:15 2019
 
 @author: chris
 """
+import timeit as time
+import numpy as np
+from itertools import product
+import matplotlib.pyplot as plt 
 
+start = time.default_timer()
 import interpreter as interpreter
 
 intcode = interpreter.preter()
 
 output = intcode.start_program("data.txt",2,0)
 print(output)
+print(time.default_timer()-start)
