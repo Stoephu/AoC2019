55239acbf4a49ef96a3d2d2676c5e646f489c451
Stoephu <christoph.straehl@gmx.ch>
2019-12-10 16:46:13 +0100

    Update asteroids.py

--- Day10/asteroids.py
+++ Day10/asteroids.py
@@ -58,34 +58,77 @@ print(max(line_of_sight_counts))
 """
 #part 2
 
-asteroid_coordinates.sort(key = lambda coord: coord[1])
+asteroid_coordinates
 
 asteroid_coordinates.remove(observation_station)
 
-rel_asteroid_coordinates = [(coord[0]-observation_station[0],coord[1]-observation_station[1]) for coord in asteroid_coordinates]
+rel_asteroid_coordinates = set([(coord[0]-observation_station[0],coord[1]-observation_station[1]) for coord in asteroid_coordinates])
 rel_slope = {}
+
 def get_slope(coord):
     if coord[1] != 0:
         return Fraction(coord[0],coord[1])
     else:
         return np.sign(coord[0])
     
-for coord in rel_asteroid_coordinates:
-    rel_slope[coord] = get_slope(coord)
-    
 def man(coord):
     return np.abs(coord[0])+np.abs(coord[1])
 
-def get_all_visible(coords):
+def get_visible(coords):
     visible = {}
     for coord in coords:
         slope = get_slope(coord)
         if slope in visible:
-            if visible[slope] > coord:
+            if man(visible[slope]) > man(coord):
                 visible[slope] = coord
         else:
             visible[slope] = coord
     return visible
 
 
+for coord in rel_asteroid_coordinates:
+    rel_slope[coord] = get_slope(coord)
+
+asteroids_hit = {}
+i = 0
+visible = get_visible(rel_asteroid_coordinates)
+def increment_i(coord):
+    global i
+    i += 1
+    asteroids_hit[i] = coord
+    
+while i <= 201:
+    right_half = []
+    middle = []
+    left_half = []
+    for slope in visible.keys():
+        
+            if slope>0:
+                right_half.append(visible[slope])
+            elif slope == 0:
+                middle.append(visible[slope])
+            else:
+                left_half.append(visible[slope])
+#    for coord in left_half:
+#        print(coord)
+#        print(np.arctan(coord[1]/coord[0]))            
+    right_half.sort(key = lambda coord: np.arctan(coord[1]/coord[0]))
+    left_half.sort(key = lambda coord: -np.arctan(coord[1]/coord[0]))
+    for coord in middle:
+        if coord[1]>0:
+            increment_i(coord)
+                
+    for coord in right_half:
+        increment_i(coord)
+    
+    for coord in middle:
+        if coord[1]<0:
+            increment_i(coord)
+    
+    for coord in left_half:
+        increment_i(coord)
+    rest = rel_asteroid_coordinates.difference(visible.values())
+    visible = get_visible(rest)
+asteroid_200 = asteroids_hit[200]
+print((asteroid_200[0]+observation_station[0],asteroid_200[1]+observation_station[1]),(asteroid_200[0]+observation_station[0])*100+(asteroid_200[1]+observation_station[1]))
 print(time.default_timer()-start)
